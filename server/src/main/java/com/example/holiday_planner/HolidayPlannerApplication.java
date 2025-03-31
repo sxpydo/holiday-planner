d7a1f417851a490c659c33af9ec467dab2b32a2b
sxpydo <shavypeynado@gmail.com>
2025-03-31 20:05:16 +0100

    initial commit to generate spring project

---
+++ server/src/main/java/com/example/holiday_planner/HolidayPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.holidayplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayPlannerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/holiday_planner/HolidayPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holidayplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
