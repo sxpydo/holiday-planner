bface15d683a9dd45380d31246b715f1687ad3d8
sxpydo <shavypeynado@gmail.com>
2025-04-01 20:02:55 +0100

    initial commit

--- server/src/main/java/com/holidayplanner/repository/FlightRepository.java
+++ server/src/main/java/com/holidayplanner/repository/FlightRepository.java
@@ -2,30 +2,9 @@ package com.holidayplanner.repository;
 
 import com.holidayplanner.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
 
-    // Find all flights from a given departure airport
-    List<Flight> findByDeparture(String departure);
-
-    // Find all flights to a given destination airport
-    List<Flight> findByDestination(String destination);
-
-    // Find a flight by departure and destination
-    Flight findByDepartureAndDestination(String departure, String destination);
-
-    List<Object[]> findShortestRoute(
-            @Param("startAirport") String startAirport,
-            @Param("endAirport") String endAirport
-    );
-
-    // Find all flights
-    @Query("SELECT f FROM Flight f")
-    List<Flight> findAllFlights();
 }
