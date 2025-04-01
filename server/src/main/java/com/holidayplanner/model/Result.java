484c6c0d75c836600d6aada8adfcf1324bcee56e
sxpydo <shavypeynado@gmail.com>
2025-04-01 13:13:40 +0100

    imported jakarta.persistence and removed javax from all relevant imports

--- server/src/main/java/com/holidayplanner/model/Result.java
+++ server/src/main/java/com/holidayplanner/model/Result.java
@@ -1,6 +1,6 @@
 package com.holidayplanner.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "results")
--- server/src/test/java/com/holidayplanner/HolidayPlannerApplicationTests.java
+++ server/src/test/java/com/holidayplanner/HolidayPlannerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.holidayplanner;
+package com.holidayplanner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
